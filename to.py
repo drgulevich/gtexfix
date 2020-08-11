2ef13a17411234bc1c2d906908d1c69f5bffb4b0
Dmitry Gulevich <drgulevich@gmail.com>
2020-08-11 08:19:11 +0300

    {align} and {subequations} environments added

--- to.py
+++ to.py
@@ -64,10 +64,10 @@ with open('gtexfix_comments', 'wb') as fp:
 start_values=[]
 end_values=[]
 for m in re.finditer(r'\\begin{ *equation\** *}|\\begin{ *figure\** *}|\\begin{ *eqnarray\** *}|\\begin{ *multline\** *}'
-    +r'|\\begin{ *thebibliography *}|\\begin{ *verbatim\** *}|\\begin{ *table\** *}',text):
+    +r'|\\begin{ *thebibliography *}|\\begin{ *verbatim\** *}|\\begin{ *table\** *}|\\begin{ *subequations\** *}|\\begin{ *align\** *}',text):
     start_values.append(m.start())
 for m in re.finditer(r'\\end{ *equation\** *}|\\end{ *figure\** *}|\\end{ *eqnarray\** *}|\\end{ *multline\** *}'
-    +r'|\\end{ *thebibliography *}|\\end{ *verbatim\** *}|\\end{ *table\** *}',text):
+    +r'|\\end{ *thebibliography *}|\\end{ *verbatim\** *}|\\end{ *table\** *}|\\end{ *subequations\** *}|\\end{ *align\** *}',text):
     end_values.append(m.end())
 nitems=len(start_values)
 assert(len(end_values)==nitems)
